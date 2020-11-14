f322077d9668c66e9c6505455551f72fb05a25a2
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-11-13 17:06:00 -0800

    Create Buttons component

---
+++ components/Buttons.js
@@ -0,0 +1,20 @@
+export default function Buttons({ keys, dispatch, activeButtons, color }) {
+    return (
+        <section className='buttons flex flex-wrap justify-evenly align-middle max-w-6xl mx-auto'>
+            {keys.map((key) => (
+                <button
+                    className={`${
+                        activeButtons.includes(key)
+                            ? `bg-${color}-500 hover:bg-${color}-400 text-white`
+                            : `bg-white text-${color}-500 hover:bg-${color}-200`
+                    } text-white text-4xl py-2 px-16 rounded-2xl mb-5 mr-8 grid place-items-center border border-4 border-${color}-500`}
+                    key={key}
+                    aria-label={`Letter ${key}`}
+                    onClick={() => dispatch({ type: "TOGGLE_BUTTON", key })}
+                >
+                    {key}
+                </button>
+            ))}
+        </section>
+    );
+}
