3fa32abff6b26cd0c0a73469e64dd1460903686e
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-11-15 13:50:41 -0800

    Add Choose word functionality

--- components/hop.js
+++ components/hop.js
@@ -2,9 +2,17 @@ import { useReducer, useState } from "react";
 import Buttons from "./Buttons";
 
 const initialState = {
+    /**
+     * Initial State of the App
+     * activeButtons: List of active Buttons
+     * words: Object of words chosen through active buttons
+     * count: count of words completed
+     * chosenWord
+     */
     activeButtons: [],
     words: {},
     count: 0,
+    chosenWord: {},
 };
 
 export default function HOP({ file }) {
@@ -13,10 +21,21 @@ export default function HOP({ file }) {
         <div className='py-5'>
             <Buttons
                 color={file.color}
-                keys={Object.keys(file.sounds)}
+                keys={Object.keys(file.words)}
                 dispatch={dispatch}
                 activeButtons={state.activeButtons}
             />
+            <p>
+                {state.chosenWord.wordCap}
+                {state.chosenWord.wordPart}
+            </p>
+            <button
+                onClick={() => {
+                    dispatch({ type: "CHOOSE_WORD" });
+                }}
+            >
+                Click
+            </button>
         </div>
     );
 }
@@ -44,27 +63,109 @@ function reducerWithFile(file) {
                     activeButtons: newActiveButtons,
                     words: {
                         ...state.words,
-                        [action.key]: file.sounds[action.key],
+                        [action.key]: transformWords(file.words[action.key]),
                     },
                 };
 
             case "UPDATE_COUNT":
-                return { ...state };
+                return { ...state, count: state.count + 1 };
             case "RESET_COUNT":
                 return { ...state, count: 0 };
             case "RESET_STATE":
                 return initialState;
+            case "CHOOSE_WORD":
+                if (Object.keys(state.words).length < 1) {
+                    console.log("empty word");
+                    return { ...state, chosenWord: {} };
+                }
+                const [
+                    newChosenWord,
+                    newWords,
+                    removeActiveFocus,
+                ] = chooseRandomWord(state.words);
+                if (removeActiveFocus !== "") {
+                    return {
+                        ...state,
+                        chosenWord: newChosenWord,
+                        words: newWords,
+                        activeButtons: state.activeButtons.filter(
+                            (b) => b !== removeActiveFocus
+                        ),
+                    };
+                }
+                return { ...state, chosenWord: newChosenWord, words: newWords };
             default:
                 return state;
         }
     };
+}
 
-    function addOrRemoveKey(array, value) {
-        var index = array.indexOf(value);
-        if (index === -1) {
-            return [[...array, value], false];
-        } else {
-            return [array.filter((arrValue) => arrValue !== value), true];
-        }
+function addOrRemoveKey(array, value) {
+    var index = array.indexOf(value);
+    if (index === -1) {
+        return [[...array, value], false];
+    } else {
+        return [array.filter((arrValue) => arrValue !== value), true];
     }
+}
+
+function transformWords(keyFocusObject) {
+    const wordPartArray = Object.keys(keyFocusObject);
+    let wordsArray = [];
+    for (let word_part of wordPartArray) {
+        const { wordCaps, ...rest } = keyFocusObject[word_part];
+        const fullWords = wordCaps.map((wordCap) => {
+            return {
+                wordCap,
+                ...rest,
+                wordPart: word_part,
+            };
+        });
+        wordsArray = [...wordsArray, ...fullWords];
+    }
+    return wordsArray;
+}
+
+function chooseRandomWord(words) {
+    //Choose random key focus
+    // Put the focus of each words into an array
+    const wordKeys = Object.keys(words);
+    // Generate random number
+    const randKeyNum = Math.floor(Math.random() * wordKeys.length);
+    // Choose key with random number
+    const chosenFocus = wordKeys[randKeyNum];
+    const wordsArray = words[chosenFocus]; // Array
+    //Choose random word from array
+    const chosenWord =
+        wordsArray[Math.floor(Math.random() * wordsArray.length)];
+
+    const { [chosenFocus]: focus, ...otherFocuses } = words;
+
+    // Filter out chosen word from wordsArray
+    const newWordsArray = wordsArray.filter((word) => {
+        if (word.wordPart === chosenWord.wordPart) {
+            if (word.wordCap === chosenWord.wordCap) {
+                return false;
+            }
+        }
+        return true;
+    });
+
+    if (newWordsArray.length < 1) {
+        return [
+            chosenWord,
+            {
+                ...otherFocuses,
+            },
+            chosenFocus,
+        ];
+    }
+    return [
+        chosenWord,
+        {
+            ...words,
+            [chosenFocus]: newWordsArray,
+        },
+        "",
+    ];
 }
