8cade8a9c8262b74f4e7c4e6774b5f65cb6848e9
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-11-15 19:31:24 -0800

    edit WordDisplay component

--- components/WordDisplay.js
+++ components/WordDisplay.js
@@ -1,33 +1,48 @@
-export default function WordDisplay({ chosenWord }) {
+export default function WordDisplay({ chosenWord, color }) {
     return (
-        <div className='flex p-10 mx-20 my-4'>
+        <div className='wordCards md:p-10 '>
             {(function () {
+                if (Object.keys(chosenWord).length < 1) {
+                    return <SingleChosenWord wordCap='Begin' />;
+                }
                 if (chosenWord.hasOwnProperty("single")) {
                     return <SingleChosenWord wordCap={chosenWord.wordCap} />;
                 }
-                return <ChosenWord chosenWord={chosenWord} />;
+                return <ChosenWord chosenWord={chosenWord} color={color} />;
             })()}
         </div>
     );
 }
 
-function ChosenWord({ chosenWord }) {
+function ChosenWord({ chosenWord, color }) {
     if (chosenWord.partPosition == "start") {
         return (
             <>
-                <div>{chosenWord.wordPart}</div>
-                <div>{chosenWord.wordCap}</div>
+                <div className='wordCard mr-6'>
+                    <div className={`start text-${color}-500`}>
+                        {chosenWord.wordPart}
+                    </div>
+                </div>
+                <div className='wordCard'>
+                    <div className='end'>{chosenWord.wordCap}</div>
+                </div>
             </>
         );
     }
     return (
         <>
-            <div>{chosenWord.wordCap}</div>
-            <div>{chosenWord.wordPart}</div>
+            <div className='wordCard mr-6'>
+                <div className='start'>{chosenWord.wordCap}</div>
+            </div>
+            <div className='wordCard'>
+                <div className={`end text-${color}-500`}>
+                    {chosenWord.wordPart}
+                </div>
+            </div>
         </>
     );
 }
 
 function SingleChosenWord({ wordCap }) {
-    return <div className='max-w-lg'>{wordCap}</div>;
+    return <div className='wordCard single'>{wordCap}</div>;
 }
