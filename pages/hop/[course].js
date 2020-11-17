9b5d165e5e31c95ed43f04177dd7be08ca96a1ca
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-11-16 17:28:00 -0800

    import Head component

--- pages/hop/[course].js
+++ pages/hop/[course].js
@@ -1,11 +1,18 @@
 import HOP from "../../components/hop";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Course({ course, file }) {
     return (
         <div className='h-screen'>
+            <Head>
+                <title>{course}</title>
+                <link rel='shortcut icon' type='image/png' href='/abc.png' />
+            </Head>
             <Link href='/'>
-                <a>Home</a>
+                <a
+                    className={`block bg-arrow bg-contain bg-no-repeat bg-${file.color}-500 w-12 h-12 rounded-full`}
+                ></a>
             </Link>
             <h1 className='text-5xl text-black'>Course: {course}</h1>
             <HOP file={file} />
