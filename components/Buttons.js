620c63ecedd0d9469eba53172cf50bd16fdaee3c
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-11-17 20:41:52 -0800

    remove margin right from each button

--- components/Buttons.js
+++ components/Buttons.js
@@ -7,7 +7,7 @@ export default function Buttons({ keys, dispatch, activeButtons, color }) {
                         activeButtons.includes(key)
                             ? `bg-${color}-500 hover:bg-${color}-400 text-gray-100`
                             : `bg-gray-100 text-${color}-500 hover:bg-${color}-200`
-                    } text-gray-100 text-3xl md:text-4xl py-2 px-12 md:px-16 rounded-2xl mb-5 mr-8 grid place-items-center border-2 border-${color}-500`}
+                    } text-gray-100 text-3xl md:text-4xl py-2 px-12 md:px-16 rounded-2xl mb-5 grid place-items-center border-2 border-${color}-500`}
                     key={key}
                     aria-label={`Letter ${key}`}
                     onClick={() => dispatch({ type: "TOGGLE_BUTTON", key })}
