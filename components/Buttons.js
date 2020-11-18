b75d9a78dc09e5fdae36a9c6edd180a98cb8a275
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-11-17 20:05:30 -0800

    undo color change

--- components/Buttons.js
+++ components/Buttons.js
@@ -5,9 +5,9 @@ export default function Buttons({ keys, dispatch, activeButtons, color }) {
                 <button
                     className={`${
                         activeButtons.includes(key)
-                            ? `bg-${color}-500 hover:bg-${color}-400`
-                            : `text-${color}-500 hover:bg-${color}-200`
-                    } text-3xl md:text-4xl py-2 px-12 md:px-16 rounded-2xl mb-5 mr-8 grid place-items-center border-2 border-${color}-500`}
+                            ? `bg-${color}-500 hover:bg-${color}-400 text-gray-100`
+                            : `bg-gray-100 text-${color}-500 hover:bg-${color}-200`
+                    } text-gray-100 text-3xl md:text-4xl py-2 px-12 md:px-16 rounded-2xl mb-5 mr-8 grid place-items-center border-2 border-${color}-500`}
                     key={key}
                     aria-label={`Letter ${key}`}
                     onClick={() => dispatch({ type: "TOGGLE_BUTTON", key })}
