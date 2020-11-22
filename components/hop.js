f7800f879d5edf4c98ea326c8f832f6ac6dcdfa1
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-11-21 19:10:23 -0800

    add reset button and counter

--- components/hop.js
+++ components/hop.js
@@ -6,7 +6,17 @@ import { reducer, initialState } from "../utils/hooks/stateReducer";
 export default function HOP({ file, playAudio }) {
     const [state, dispatch] = useReducer(reducer(file), initialState);
     return (
-        <div className='py-5'>
+        <div className='pb-24 relative'>
+            <div className='options absolute bottom-8 right-20'>
+                <button
+                    className='bg-amber-300 hover:bg-amber-400 text-white text-2xl px-5 py-2'
+                    onClick={() => {
+                        dispatch({ type: "RESET_STATE" });
+                    }}
+                >
+                    reset
+                </button>
+            </div>
             <Buttons
                 color={file.color}
                 keys={Object.keys(file.words)}
@@ -14,6 +24,12 @@ export default function HOP({ file, playAudio }) {
                 activeButtons={state.activeButtons}
             />
             <WordDisplay chosenWord={state.chosenWord} color={file.color} />
+            <section className='counter grid place-items-center'>
+                <span className='text-2xl'>Word Counter</span>
+                <h2 className='text-gray-600 inline-block text-6xl'>
+                    {state.count}{" "}
+                </h2>
+            </section>
             <button
                 className={`block text-3xl py-3 px-8 mx-auto my-8 rounded-md bg-gray-100 hover:bg-gray-200 text-${file.color}-500`}
                 onClick={() => {
