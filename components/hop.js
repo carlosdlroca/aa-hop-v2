cb8dd9d865ddc6a3b20228a632023c61bda85ee0
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-11-28 21:19:41 -0800

    add media query to reset button

--- components/hop.js
+++ components/hop.js
@@ -7,7 +7,7 @@ export default function HOP({ file, playAudio }) {
     const [state, dispatch] = useReducer(reducer(file), initialState);
     return (
         <div className='pb-24 relative'>
-            <div className='options absolute bottom-8 right-20'>
+            <div className='options absolute bottom-1 right-10 md:bottom-8 md:right-20'>
                 <button
                     className='bg-yellow-300 hover:bg-yellow-400 text-white text-2xl px-5 py-2'
                     onClick={() => {
